49e9db660ad7fe60c7cd81cbff5d9e93ac7c2edb
Robert Evans <evans.robert.r@gmail.com>
2021-02-04 17:48:01 -0500

    Recreated all the model classes. Added classes for cart items and shopping cart. Added class for customer. Created pages for current roasts, upcoming roasts, and view roast. Added form to add roast to cart.

--- Flight_School_Coffee_app/urls.py
+++ Flight_School_Coffee_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('contact_us', views.contact_us),
     path('about_us', views.about_us),
+    path('current_roasts', views.current_roasts),
+    path('upcoming_roasts', views.upcoming_roasts),
+    path('<int:roast_id>/view_roast', views.view_roast),
+    path('<int:roast_id>/add_to_cart', views.add_to_cart),
 ]
